703eff3c9423629acfda250a11aeb7ac444c2e93
Serhii Harmash <Serhii_Harmash@epam.com>
2019-05-24 07:55:48 +0300

    Add project files.

---
+++ testDelete/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace testDelete
+{
+    class Program
+    {
+        public static event EventHandler MyEvent //публичное событие MyEvent с типом EventHandler
+        {
+            add
+            {
+                Console.WriteLine("add operation");
+            }
+
+            remove
+            {
+                Console.WriteLine("remove operation");
+            }
+        }
+
+        static void Main()
+        {
+        
+
+            MyEvent += new EventHandler(DoNothing);
+            MyEvent -= null;
+
+            Console.ReadKey();
+        }
+
+        static void DoNothing(object sender, EventArgs e)
+        {
+        }
+
+        static void Print(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item+ " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+    
+}
